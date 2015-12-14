02b858fcef2e336c5bc20f6a67ce95ad694104cf
Nekith <nekith@gmail.com>
2015-12-14 19:55:21 +0100

    Parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour
+{
+    public float effectFactor;
+
+    Transform camera;
+    Vector3 previousPosition;
+
+    void Start()
+    {
+        camera = Camera.main.transform;
+        previousPosition = camera.position;
+    }
+
+    void Update()
+    {
+        Vector3 distance = camera.position - previousPosition;
+        transform.position += Vector3.Scale(distance, new Vector3(effectFactor, effectFactor)) * -1;
+        previousPosition = camera.position;
+    }
+}
