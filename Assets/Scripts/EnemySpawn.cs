b4948820e5b8a30a56fd637453693f7a0857e36e
Nekith <nekith@gmail.com>
2015-12-17 17:28:50 +0100

    Sync boss spawn with animation

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -30,11 +30,15 @@ public class EnemySpawn : MonoBehaviour
             cooldown -= Time.fixedDeltaTime;
         }
         if (cooldown <= 0f) {
+            if (onSite) {
+                StartCoroutine(BossAnimation());
+            }
             for (int i = 0; i < numberBySpawn; i++) {
                 Vector3 pos = new Vector3(0f, 0f, transform.position.z);
                 if (onSite) {
                     pos.x = transform.position.x;
                     pos.y = transform.position.y;
+                    StartCoroutine(SpawnAnimation(pos));
                 } else {
                     while (true) {
                         pos.x = Random.Range(min.x, max.x);
@@ -44,17 +48,41 @@ public class EnemySpawn : MonoBehaviour
                             break;
                         }
                     }
+                    GameObject obj;
+                    int r = Random.Range(0, 2);
+                    if (r == 0) {
+                        obj = GameObject.Instantiate(Resources.Load("RedEnemy") as GameObject);
+                    } else {
+                        obj = GameObject.Instantiate(Resources.Load("GreenEnemy") as GameObject);
+                    }
+                    obj.transform.position = pos;
                 }
-                GameObject obj;
-                int r = Random.Range(0, 2);
-                if (r == 0) {
-                    obj = GameObject.Instantiate(Resources.Load("RedEnemy") as GameObject);
-                } else {
-                    obj = GameObject.Instantiate(Resources.Load("GreenEnemy") as GameObject);
-                }
-                obj.transform.position = pos;
                 cooldown = cooldownDuration;
             }
         }
+    }
+
+    IEnumerator BossAnimation()
+    {
+        Debug.Log(transform.name);
+        Debug.Log(transform.parent.name);
+        Debug.Log(transform.parent.parent.name);
+        Animator tongue = transform.parent.parent.gameObject.GetComponent<Animator>();
+        tongue.SetBool("Spawn", true);
+        yield return new WaitForSeconds(0.8f);
+        tongue.SetBool("Spawn", false);
+    }
+
+    IEnumerator SpawnAnimation(Vector3 pos)
+    {
+        yield return new WaitForSeconds(0.8f);
+        GameObject obj;
+        int r = Random.Range(0, 2);
+        if (r == 0) {
+            obj = GameObject.Instantiate(Resources.Load("RedEnemy") as GameObject);
+        } else {
+            obj = GameObject.Instantiate(Resources.Load("GreenEnemy") as GameObject);
+        }
+        obj.transform.position = pos;
     }
 }
